044585f76e93f9a70b6150f7ed654bfd10c77c07
hinioa <1271889901@qq.com>
2021-01-30 07:00:08 +0800

    create: 创建职位列表页面和职位申请页面组件

---
+++ src/views/JobApply.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    职位申请
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'JobApply'
+})
+</script>
