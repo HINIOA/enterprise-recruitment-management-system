86d03c13d4b3da6e36e25b0e936ed0607aa1f136
hinioa <1271889901@qq.com>
2021-01-30 20:58:50 +0800

    feat: 实现职位列表页面结构及样式

--- src/views/JobList.vue
+++ src/views/JobList.vue
@@ -1,13 +1,141 @@
 <template>
-  <div>
-    职位列表
+  <div class="job-list__container">
+    <el-card class="job-list__filter">
+      <p class="job-list__filter-title">职位筛选</p>
+      <base-search
+        placeholder="输入职位关键字"
+        buttonText="搜索职位"
+        @search="searchHandler"
+      ></base-search>
+    </el-card>
+    <el-card class="job-list__content">
+      <div class="job-list__item" v-for="job in jobList" :key="job.id">
+        <div class="job-list__item-container">
+          <p class="job-list__item-name">{{ job.name }}</p>
+        <p class="job-list__item-info--middle">{{ job.types.join('｜') }}</p>
+        <div class="job-list__item-info--bottom">
+          <p class="job-list__item-info--position">{{ job.position }}</p>
+          <p class="job-list__item-info--time">发布时间：{{ job.time }}</p>
+        </div>
+        </div>
+      </div>
+    </el-card>
+    <div class="job-list__pagination">
+      <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import BaseSearch from "../components/BaseSearch.vue";
 
 export default defineComponent({
-  name: 'JobList'
-})
+  name: "JobList",
+  components: {
+    baseSearch: BaseSearch,
+  },
+  setup() {
+    const jobList = ref([
+      {id: '1', name: '前端开发工程师', types: ['全职','研发类'], position: '广东·深圳市', time: '2021-01-30'},
+      {id: '2', name: '前端开发工程师', types: ['全职','研发类'], position: '广东·深圳市', time: '2021-01-30'},
+      {id: '3', name: '前端开发工程师', types: ['全职','研发类'], position: '广东·深圳市', time: '2021-01-30'},
+      {id: '4', name: '前端开发工程师', types: ['全职','研发类'], position: '广东·深圳市', time: '2021-01-30'},
+      {id: '5', name: '前端开发工程师', types: ['全职','研发类'], position: '广东·深圳市', time: '2021-01-30'},
+    ])
+    const searchHandler = (input: string) => {
+      console.log(input);
+    };
+
+    return {
+      jobList,
+      searchHandler,
+    };
+  },
+});
 </script>
+
+<style lang="scss">
+.job-list {
+  &__container {
+    padding: 2rem;
+  }
+
+  &__filter {
+    margin-bottom: 1rem;
+
+    &-title {
+      font-size: 1.2rem;
+      margin-bottom: 1rem;
+    }
+  }
+
+  &__content {
+    margin-bottom: 2rem;
+
+    .el-card__body {
+      padding: 0 !important;
+    }
+  }
+
+  &__item {
+    padding: 0 2rem;
+    cursor: pointer;
+
+    &:last-of-type {
+      border-bottom: 0;
+    }
+
+    &:hover {
+      background-color: rgb(249,249,250);
+    }
+
+    &-container {
+      padding: 1.5rem 0;
+      border-bottom: 1px solid rgb(238, 238, 238);
+    }
+
+    &-name {
+      max-width: 20rem;
+      font-size: 1.2rem;
+      font-weight: bold;
+      line-height: 1.5rem;
+      color: #222831;
+      margin-bottom: 0.8rem;
+    }
+
+    &-info {
+      &--middle {
+        font-size: 1rem;
+        line-height: 1.5rem;
+        color: #a2a4a8;
+        margin-bottom: 0.8rem;
+      }
+
+      &--bottom {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      &--position {
+        font-size: 1rem;
+        color: #575d6a;
+      }
+
+      &--time {
+        font-size: 0.8rem;
+        color: #a2a4a8;
+      }
+    }
+  }
+
+  &__pagination {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
