088aca6411249112db1bc47a924f4c6242e6e916
hinioa <1271889901@qq.com>
2021-01-31 21:56:30 +0800

    style: 优化基础标题卡片组件 CSS 类名

--- src/components/BaseTitleCard.vue
+++ src/components/BaseTitleCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container">
-    <div class="title">
-      <h3 class="title-text">{{ title }}</h3>
-      <div class="title-underline"></div>
+  <div class="title-card__container">
+    <div class="title-card__header">
+      <h3 class="title-card__text">{{ title }}</h3>
+      <div class="title-card__underline"></div>
     </div>
     <slot></slot>
   </div>
@@ -20,29 +20,31 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.container {
-  padding: 4rem 2rem;
+.title-card {
+  &__container {
+    padding: 4rem 2rem;
+  }
 
-  .title {
+  &__header {
     position: relative;
     margin-bottom: 4rem;
+  }
 
-    &-text {
-      text-align: center;
-      font-size: 2rem;
-      font-weight: 400;
-      color: rgba(0, 0, 0, 0.8);
-    }
+  &__text {
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.8);
+  }
 
-    &-underline {
-      position: absolute;
-      left: 50%;
-      bottom: -20px;
-      margin-left: -40px;
-      width: 80px;
-      height: 2px;
-      background-color: rgba(0, 0, 0, 0.05);
-    }
+  &__underline {
+    position: absolute;
+    left: 50%;
+    bottom: -20px;
+    margin-left: -40px;
+    width: 80px;
+    height: 2px;
+    background-color: rgba(0, 0, 0, 0.05);
   }
 }
 </style>
