93eac4a3ee2508063599682d97422a9b3c84c2ee
hinioa <1271889901@qq.com>
2021-01-31 22:42:15 +0800

    feat: 实现点击跳转功能 & 优化 CSS 类名

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="home">
     <!-- Banner -->
-    <el-carousel height="33.3vw" :autoplay="false">
+    <el-carousel height="33.3vw" :autoplay="false" class="banner">
       <el-carousel-item v-for="url in bannerUrls" :key="url">
-        <img :src="url" alt="banner" class="banner" />
+        <img :src="url" alt="banner" class="banner__image" />
       </el-carousel-item>
     </el-carousel>
     <!-- 开放职位 -->
-    <title-card title="开放职位" class="jobs">
-      <div class="search">
+    <div class="jobs">
+      <title-card title="开放职位" class="jobs__container">
+      <div class="jobs__search">
         <el-input v-model="searchInput" placeholder="请输入职位关键字" />
-        <el-button type="primary">搜索职位</el-button>
+        <el-button type="primary" @click="searchJob">搜索职位</el-button>
       </div>
-      <div class="list">
+      <div class="jobs__content">
         <el-row v-for="(row, idx) in jobListRows" :key="idx" :gutter="10">
           <el-col v-for="item in row" :key="item" :span="8">
-            <div class="item">
-              <router-link to="/job-apply" class="link">
-                <p class="name">{{ item.name }}</p>
-                <p class="info">{{ item.info }}</p>
+            <div class="jobs__item">
+              <router-link :to="/job-detail/ + item.id" class="jobs__item-link">
+                <p class="jobs__item-name">{{ item.name }}</p>
+                <p class="jobs__item-info">{{ item.info }}</p>
               </router-link>
             </div>
           </el-col>
         </el-row>
       </div>
-      <div class="more">
+      <div class="jobs__more">
         <el-button type="primary" @click="routeToJobList">查看更多职位</el-button>
       </div>
     </title-card>
+    </div>
+    
     <!-- 关于 -->
     <title-card title="关于 Shopee" class="about">
       <div class="about-container">
@@ -39,31 +42,31 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 import BaseTitleCard from "@/components/BaseTitleCard.vue";
 
 function getJobListRows() {
   const data = [
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-    { name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
-  ]
-  const jobListRows = []
+    { id: "1", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "2", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "3", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "4", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "5", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "6", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "7", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "8", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "9", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "10", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "11", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+    { id: "12", name: "前端开发工程师", info: "研发类 | 广东·深圳市" },
+  ];
+  const jobListRows = [];
 
-  for (let i = 0; i + 2 < data.length; i+=3) {
-    jobListRows.push([data[i], data[i + 1], data[i + 2]])
+  for (let i = 0; i + 2 < data.length; i += 3) {
+    jobListRows.push([data[i], data[i + 1], data[i + 2]]);
   }
 
-  return ref(jobListRows)
+  return ref(jobListRows);
 }
 
 export default defineComponent({
@@ -77,13 +80,18 @@ export default defineComponent({
       "https://img.imgdb.cn/item/601503083ffa7d37b3cca9b7.jpg",
       "https://img.imgdb.cn/item/601502bd3ffa7d37b3cc930f.jpg",
     ]);
-    const jobListRows = getJobListRows()
-    const aboutText = 'Shopee是东南亚领航电商平台，覆盖新加坡、马来西亚、菲律宾、印度尼西亚、泰国和越南等市场。同时在中国深圳、上海和香港设立跨境业务办公室。2019年 Shopee总订单量高达12亿，同比增长100.5%。根据权威移动数据分析平台 App Annie，2019年Shopee强势跻身全球购物类App下载量前五，同时斩获东南亚及台湾市场购物类App年度总下载量、平均月活数、安卓使用总时长三项冠军，并领跑东南亚两大头部市场，拿下印尼及越南年度购物类App下载量与月活数双冠王。'
-    const searchInput = ref('')
+    const jobListRows = getJobListRows();
+    const aboutText =
+      "Shopee是东南亚领航电商平台，覆盖新加坡、马来西亚、菲律宾、印度尼西亚、泰国和越南等市场。同时在中国深圳、上海和香港设立跨境业务办公室。2019年 Shopee总订单量高达12亿，同比增长100.5%。根据权威移动数据分析平台 App Annie，2019年Shopee强势跻身全球购物类App下载量前五，同时斩获东南亚及台湾市场购物类App年度总下载量、平均月活数、安卓使用总时长三项冠军，并领跑东南亚两大头部市场，拿下印尼及越南年度购物类App下载量与月活数双冠王。";
+    const searchInput = ref("");
 
-    const router = useRouter()
+    const router = useRouter();
     const routeToJobList = () => {
-      router.push('/jobs')
+      router.push("/jobs");
+    };
+
+    const searchJob = () => {
+      routeToJobList()
     }
 
     return {
@@ -92,22 +100,26 @@ export default defineComponent({
       jobListRows,
       aboutText,
       routeToJobList,
+      searchJob,
     };
   },
 });
 </script>
 
 <style lang="scss">
-.el-carousel__item {
-  .banner {
-    height: 100%;
-  }
+.banner-image {
+  height: 100%;
 }
 
 .jobs {
   background-color: rgba(0, 0, 0, 0.05);
 
-  .search {
+  &__container {
+    max-width: 75rem;
+    margin: 0 auto;
+  }
+
+  &__search {
     display: flex;
     width: 50%;
     margin: 0 auto;
@@ -123,40 +135,43 @@ export default defineComponent({
     }
   }
 
-  .list {
+  &__content {
     padding: 2rem;
 
     .el-row {
       margin-bottom: 10px;
     }
+  }
 
-    .item {
-      background-color: #fff;
+  &__item {
+    background-color: #fff;
 
-      .link {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        padding: 2rem 4rem 2rem 2rem;
-        height: 6rem;
-        text-decoration: none;
+    &:hover {
+      opacity: 0.7;
+    }
 
-        .name {
-          font-size: 1.5rem;
-          font-weight: 400;
-          line-height: 2rem;
-          color: rgb(41, 44, 50);
-        }
+    &-link {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 2rem 4rem 2rem 2rem;
+      text-decoration: none !important;
+    }
 
-        .info {
-          font-size: 1rem;
-          color: rgb(162, 164, 168);
-        }
-      }
+    &-name {
+      font-size: 1.5rem;
+      font-weight: 400;
+      line-height: 2rem;
+      color: rgb(41, 44, 50);
+    }
+
+    &-info {
+      font-size: 1rem;
+      color: rgb(162, 164, 168);
     }
   }
 
-  .more {
+  &__more {
     display: flex;
     justify-content: center;
   }
